ff5ed8ecbc6533b346ddcf9de63c3b7f98e474b8
ali <aliburak.yilmaz@outlook.com>
2021-04-07 01:27:00 +0300

    some edits

--- DataProcessor/FileProcessor.cs
+++ DataProcessor/FileProcessor.cs
@@ -87,6 +87,7 @@ namespace DataProcessor
             var completedFilePath = Path.Combine(completedDirectoryPath, completedFileName);
             File.Move(inProgressFilePath, completedFilePath);
             string inProgressDirectoryPath = Path.GetDirectoryName(inProgressFilePath);
+            Directory.Delete(inProgressDirectoryPath, true);
         }
 
         private void ProcessTextFile(string inProgressFilePath)
