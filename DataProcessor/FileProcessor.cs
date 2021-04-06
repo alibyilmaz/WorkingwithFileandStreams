e6dd6bb3a3d05e336c84f4fba2cc361beeb29f0f
ali <ali.yilmaz@alternacx.com>
2021-04-06 23:26:17 +0300

    data processor for control exist file

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+using System.IO;
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        public string InputFilePath { get; }
+
+        public FileProcessor(string filePath)
+        {
+            InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            WriteLine($"begin process of {InputFilePath}");
+        }
+    }
+}
